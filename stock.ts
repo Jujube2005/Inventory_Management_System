e1ce3209a07f166e9643ecb3a0ae1afa9851cdf0
jujubea <chotika.jakchai@gmail.com>
2025-09-21 15:25:00 +0700

    Update stock.ts

--- stock.ts
+++ stock.ts
@@ -1,4 +1,21 @@
+import { Product } from './product';
+
 export class Stock {
     public idstock: number;
-    public 
+    public quantity: number;
+    public location: string;
+    public product: Product;
+
+    constructor(idstock: number, quantity: number, location: string, product: Product) {
+    this.idstock = idstock;
+    this.quantity = quantity;
+    this.location = location;
+    this.product = product;
+  }
+
+  public updateStock(newQuantity: number): void {
+    this.quantity = newQuantity;
+  }
+}
+
 }
